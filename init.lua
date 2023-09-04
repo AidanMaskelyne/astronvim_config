9deec35ba51dd1d28d71740e67965b0572f9322e
Aidan <aidan@muttleyville.org>
2023-09-04 16:36:58 +0100

    Change theme to astrodark

--- init.lua
+++ init.lua
@@ -18,8 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  -- colorscheme = "astrodark",
-  colorscheme = "one_monokai",
+  colorscheme = "astrodark",
+  -- colorscheme = "one_monokai",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
